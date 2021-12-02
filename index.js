ea08fa088da2ee818933a5398325e9b062196c5e
Austin Longo <austinlongocontracting@gmail.com>
2021-12-02 15:09:32 -0500

    commit tree ad119369de5c44c3fa94eb67d45b929c9bf5fdd1

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(data) {return data.slice(0,2)};
+
+const returnLastTwoDrivers = function(data) {return data.slice(data.length -2,data.length)};
+
+let selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier = function(multiplier) {return function(fare) {return fare * multiplier }};
+
+const fareDoubler = function(fare) {return createFareMultiplier(2)(fare)};
+const fareTripler = function(fare) {return createFareMultiplier(3)(fare)};
+
+
+function selectDifferentDrivers(arrayOfDrivers, fun) {
+    return fun(arrayOfDrivers);
+}
+
